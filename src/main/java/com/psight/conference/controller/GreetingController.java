8c7f549a834f2ab68e7bae51374fe12fbc8e7e6f
Brian Hanna <bhanna1693@gmail.com>
2020-04-06 20:19:17 -0400

    added thymeleaf dependency to pom.xml, created a thyme template and added necessary Beans to ConferenceConfig file to use thymeleaf

--- src/main/java/com/psight/conference/controller/GreetingController.java
+++ src/main/java/com/psight/conference/controller/GreetingController.java
@@ -9,8 +9,14 @@ import java.util.Map;
 public class GreetingController {
 
     @GetMapping("greeting")
-    public String greeting (Map<String, Object> model) {
+    public String greeting(Map<String, Object> model) {
         model.put("message", "Hello Brian");
         return "greeting";
+    }
+
+    @GetMapping("thyme")
+    public String thyme(Map<String, Object> model) {
+        model.put("message", "Hello Thyme");
+        return "thyme";
     }
 }
